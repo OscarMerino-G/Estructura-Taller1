5a4bd74e81799b2b2d58eb2a4807bb1c3495982f
Oscar <oscar.merino@alumnos.ucn.cl>
2026-09-18 16:08:18 -0300

    Update persona.hpp
    creado destructor y comentado

--- Taller1/include/clases/persona.hpp
+++ Taller1/include/clases/persona.hpp
@@ -7,7 +7,12 @@ protected:
     int edad;
 
 public:
+    // constructor
     Persona(std::string n, int e) : nombre(n), edad(e) {}
+
+    // getts
     std::string getNombre() const { return nombre; }
     int getEdad() const { return edad; }
+
+    virtual ~Persona() {} // destructor 
 };
