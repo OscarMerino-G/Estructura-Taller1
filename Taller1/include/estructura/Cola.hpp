c6ca5a15306df68bafb0ec6e8ebfd20324072904
Oscar <oscar.merino@alumnos.ucn.cl>
2026-09-18 18:15:02 -0300

    Update and rename cola.txt to Cola.hpp

---
+++ Taller1/include/estructura/Cola.hpp
@@ -0,0 +1,36 @@
+#ifndef COLA_HPP
+#define COLA_HPP
+
+#include "LinkedList.hpp"
+
+template <typename T>
+class Cola {
+private:
+    Lista<T> list;
+    
+public:
+    Cola();
+    ~Cola();
+    
+    bool isEmpty() const;
+    size_t size() const;
+    void clear();
+    
+    void encolar(const T& data);
+    void deencolar();
+    T& front();
+    const T& front() const;
+    T& back();
+    const T& back() const;
+    
+    
+    T& at(size_t index);
+    const T& at(size_t index) const;
+    void insertAt(size_t index, const T& data); // Inserta el elemento en X posicion
+    void removeAt(size_t index); // Elimina el elemento en la posicon X
+    LinkedList<T>& getList();
+    const LinkedList<T>& getList() const;
+};
+
+#include "../../src/Estructura_Datos/Cola.cpp"
+#endif
