b7a79e5fc865b9b4acf1708dd2304a0973db90e4
Oscar <oscar.merino@alumnos.ucn.cl>
2026-09-18 16:07:22 -0300

    Update persona.hpp
    creado persona hpp

--- Taller1/include/clases/persona.hpp
+++ Taller1/include/clases/persona.hpp
@@ -1,1 +1,13 @@
 // persona hpp
+#include <string>
+
+class Persona {
+protected:
+    std::string nombre;
+    int edad;
+
+public:
+    Persona(std::string n, int e) : nombre(n), edad(e) {}
+    std::string getNombre() const { return nombre; }
+    int getEdad() const { return edad; }
+};
